ec18f2ece48933cfc294e80279fd2ba18ae796d7
Rafael Sachetto <rsachetto@gmail.com>
2017-10-19 13:49:33 -0200

    Code cleanup and Human mesh implementation!

---
+++ src/domains_library/domain_helpers.h
@@ -0,0 +1,27 @@
+//
+// Created by sachetto on 19/10/17.
+//
+
+#ifndef MONOALG3D_DOMAIN_HELPERS_H
+#define MONOALG3D_DOMAIN_HELPERS_H
+
+#include "../alg/grid/grid.h"
+
+int get_num_refinement_steps_to_discretization (double side_len, double h);
+
+void set_benchmark_domain (struct grid *the_grid);
+void set_plain_domain (struct grid *the_grid, double sizeX, double sizeY, double sizeZ);
+
+void set_custom_mesh (struct grid *the_grid, const char *file_name, int size);
+
+void set_custom_mesh_with_bounds (struct grid *the_grid, const char *file_name, int size, double minx, double maxx,
+                                  double miny, double maxy, double minz, double maxz);
+
+void set_cell_not_changeable (struct cell_node *c, double initialDiscretization);
+
+void set_plain_fibrosis (struct grid *the_grid, double phi, unsigned fib_seed);
+
+void set_plain_sphere_fibrosis (struct grid *the_grid, double phi, double plain_center, double sphere_radius,
+                                double bz_size, double bz_radius, unsigned fib_seed);
+
+#endif // MONOALG3D_DOMAIN_HELPERS_H
--- src/domains_library/grid_domain.h
+++ src/domains_library/grid_domain.h
@@ -4,9 +4,6 @@
 #include "../main/config/domain_config.h"
 #include "../alg/grid/grid.h"
 
-
-
-
 void initialize_grid_with_mouse_mesh (struct grid *the_grid, struct domain_config *domain_config);
 void initialize_grid_with_rabbit_mesh (struct grid *the_grid, struct domain_config *domain_config);
 void initialize_grid_with_benchmark_mesh (struct grid *the_grid, struct domain_config *domain_config);
@@ -14,13 +11,5 @@ void initialize_grid_with_benchmark_mesh (struct grid *the_grid, struct domain_c
 void initialize_grid_with_plain_mesh (struct grid *the_grid, struct domain_config *domain_config);
 void initialize_grid_with_plain_fibrotic_mesh(struct grid *the_grid, struct domain_config* config);
 void initialize_grid_with_plain_and_sphere_fibrotic_mesh(struct grid *the_grid, struct domain_config* config);
-
-
-//Helper functions
-void set_plain_sphere_fibrosis(struct grid* the_grid, double phi,  double plain_center, double sphere_radius, double bz_size,
-                               double bz_radius, unsigned fib_seed);
-void set_plain_fibrosis(struct grid* the_grid, double phi, unsigned fib_seed);
-int get_num_refinement_steps_to_discretization (double side_len, double h);
-
 
 #endif //MONOALG3D_GRID_DOMAIN_H
