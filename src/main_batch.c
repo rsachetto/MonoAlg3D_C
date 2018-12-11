84b6a5336f13eae5e6cde0e0110ca116c14c351a
Rafael Sachetto <rsachetto@gmail.com>
2018-12-11 19:24:12 -0200

    Starting to create a simulation batch system using MPI....

--- src/main_batch.c
+++ src/main_batch.c
@@ -23,7 +23,10 @@ int main(int argc, char **argv) {
 
     int simulation_number_start;
     int num_simulations;
+    int num_par_change;
+
     char *output_folder;
+
     struct string_hash *config_to_change = string_hash_create();
     char *entire_config_file = NULL;
     long config_file_size;
@@ -41,6 +44,13 @@ int main(int argc, char **argv) {
             MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
         }
 
+        entire_config_file = read_entire_file(batch_options->initial_config, &config_file_size);
+
+        if(entire_config_file == NULL) {
+            fprintf(stderr, "Error: Can't load the config file %s\n", batch_options->initial_config);
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        }
+
         config_to_change = batch_options->config_to_change;
 
         if(num_proccess > batch_options->num_simulations) {
@@ -49,20 +59,15 @@ int main(int argc, char **argv) {
             num_max_proc = num_proccess;
         }
 
-        create_dir(batch_options->output_folder);
-
         num_simulations = batch_options->num_simulations / num_max_proc;
         int last_rank_extra = batch_options->num_simulations % num_max_proc;
 
         simulation_number_start = 0;
         int size_out_folder = (int)strlen(batch_options->output_folder) + 1;
 
-        entire_config_file = read_entire_file(batch_options->initial_config, &config_file_size);
+        create_dir(batch_options->output_folder);
 
-        if(entire_config_file == NULL) {
-            fprintf(stderr, "Error: Can't load the config file %s\n", batch_options->initial_config);
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-        }
+        num_par_change = batch_options->num_par_change;
 
         // Send the data to all other process
         for(int i = 1; i < num_proccess; i++) {
@@ -80,6 +85,7 @@ int main(int argc, char **argv) {
             }
 
             MPI_Send(&num_sims, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&num_par_change, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&simulation_number_start, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&size_out_folder, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(batch_options->output_folder, size_out_folder, MPI_CHAR, i, 0, MPI_COMM_WORLD);
@@ -111,6 +117,7 @@ int main(int argc, char **argv) {
         int num_options;
 
         MPI_Recv(&num_simulations, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&num_par_change, 1, MPI_INT,  0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(&simulation_number_start, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         MPI_Recv(&size_out_folder, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -135,6 +142,8 @@ int main(int argc, char **argv) {
             MPI_Recv(value, key_value_sizes[1], MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
             string_hash_insert(config_to_change, key, value);
+
+            
 
             free(key);
             free(value);
@@ -176,78 +185,82 @@ int main(int argc, char **argv) {
     }
 
 
+    char *changed_parameters = NULL;
+
 
 
     //Parse the modification directives
     for(int s = simulation_number_start; s < simulation_number_start+num_simulations; s++) {
 
-        the_grid = new_grid();
-        ode_solver = new_ode_solver();
+        for (int p = 0; p < num_par_change; p++) {
+            the_grid = new_grid();
+            ode_solver = new_ode_solver();
 
-        sds new_out_dir_name =
-            sdscatprintf(sdsempty(), "%s/%s_%d", output_folder, options->save_mesh_config->out_dir_name, s);
+            sds new_out_dir_name =
+                    sdscatprintf(sdsempty(), "%s/%s_%d", output_folder, options->save_mesh_config->out_dir_name, s);
 
-        free(options->save_mesh_config->out_dir_name);
-        options->save_mesh_config->out_dir_name = strdup(new_out_dir_name);
-        sdsfree(new_out_dir_name);
+            free(options->save_mesh_config->out_dir_name);
+            options->save_mesh_config->out_dir_name = strdup(new_out_dir_name);
+            sdsfree(new_out_dir_name);
 
-        printf("Rank %d, performing simulation %d and saving in %s\n", rank, simulation_number_start,
-               options->save_mesh_config->out_dir_name);
+            printf("Rank %d, performing simulation %d and saving in %s\n", rank, simulation_number_start,
+                   options->save_mesh_config->out_dir_name);
 
-        // Create the output dir and the logfile
-        if(options->save_mesh_config && options->save_mesh_config->out_dir_name) {
-            sds buffer_log = sdsnew("");
-            sds buffer_ini = sdsnew("");
+            // Create the output dir and the logfile
+            if (options->save_mesh_config && options->save_mesh_config->out_dir_name) {
+                sds buffer_log = sdsnew("");
+                sds buffer_ini = sdsnew("");
 
-            create_dir(options->save_mesh_config->out_dir_name);
-            buffer_log = sdscatfmt(buffer_log, "%s/outputlog.txt", options->save_mesh_config->out_dir_name);
-            open_logfile(buffer_log);
+                create_dir(options->save_mesh_config->out_dir_name);
+                buffer_log = sdscatfmt(buffer_log, "%s/outputlog.txt", options->save_mesh_config->out_dir_name);
+                open_logfile(buffer_log);
 
-            print_to_stdout_and_file("Command line to reproduce this simulation:\n");
-            for(int i = 0; i < argc; i++) {
-                print_to_stdout_and_file("%s ", argv[i]);
+                print_to_stdout_and_file("Command line to reproduce this simulation:\n");
+                for (int i = 0; i < argc; i++) {
+                    print_to_stdout_and_file("%s ", argv[i]);
+                }
+
+                print_to_stdout_and_file("\n");
+
+                buffer_ini =
+                        sdscatfmt(buffer_ini, "%s/original_configuration.ini", options->save_mesh_config->out_dir_name);
+
+                print_to_stdout_and_file("For reproducibility purposes the configuration file was copied to file: %s\n",
+                                         buffer_ini);
+
+                cp_file(buffer_ini, options->config_file);
+
+                sdsfree(buffer_log);
+                sdsfree(buffer_ini);
             }
 
-            print_to_stdout_and_file("\n");
-
-            buffer_ini =
-                sdscatfmt(buffer_ini, "%s/original_configuration.ini", options->save_mesh_config->out_dir_name);
-
-            print_to_stdout_and_file("For reproducibility purposes the configuration file was copied to file: %s\n",
-                                     buffer_ini);
-
-            cp_file(buffer_ini, options->config_file);
-
-            sdsfree(buffer_log);
-            sdsfree(buffer_ini);
-        }
-
-        configure_ode_solver_from_options(ode_solver, options);
-        configure_monodomain_solver_from_options(monodomain_solver, options);
-        configure_grid_from_options(the_grid, options);
+            configure_ode_solver_from_options(ode_solver, options);
+            configure_monodomain_solver_from_options(monodomain_solver, options);
+            configure_grid_from_options(the_grid, options);
 
 #ifndef COMPILE_CUDA
-        if(ode_solver->gpu) {
-            print_to_stdout_and_file("Cuda runtime not found in this system. Fallbacking to CPU solver!!\n");
-            ode_solver->gpu = false;
-        }
+            if(ode_solver->gpu) {
+                print_to_stdout_and_file("Cuda runtime not found in this system. Fallbacking to CPU solver!!\n");
+                ode_solver->gpu = false;
+            }
 #endif
 
-        int nt = monodomain_solver->num_threads;
-        options->draw = false;
+            int nt = monodomain_solver->num_threads;
+            options->draw = false;
 
-        if(nt == 0)
-            nt = 1;
+            if (nt == 0)
+                nt = 1;
 
 #if defined(_OPENMP)
-        omp_set_num_threads(nt);
+            omp_set_num_threads(nt);
 #endif
 
-        solve_monodomain(monodomain_solver, ode_solver, the_grid, options);
+            solve_monodomain(monodomain_solver, ode_solver, the_grid, options);
 
-        clean_and_free_grid(the_grid);
-        free_ode_solver(ode_solver);
-        close_logfile();
+            clean_and_free_grid(the_grid);
+            free_ode_solver(ode_solver);
+            close_logfile();
+        }
     }
 
     free(monodomain_solver);
