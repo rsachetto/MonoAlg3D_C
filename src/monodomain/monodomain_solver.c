c7303571da8c44ca2a45be66554365e6d1822bad
bergolho <berg@ice.ufjf.br>
2019-01-24 12:40:03 -0200

    Add DDM implementation

--- src/monodomain/monodomain_solver.c
+++ src/monodomain/monodomain_solver.c
@@ -32,6 +32,14 @@ struct monodomain_solver *new_monodomain_solver() {
     result->current_time = 0.0;
     result->current_count = 0;
 
+    // **************************************************
+    // Berg and Pedro code ...
+    result->using_ddm = false;
+    result->kappa_x = 0.0;
+    result->kappa_y = 0.0;
+    result->kappa_z = 0.0;
+    // **************************************************
+
     return result;
 }
 
@@ -321,8 +329,17 @@ void solve_monodomain(struct monodomain_solver *the_monodomain_solver, struct od
         // REACTION
         solve_all_volumes_odes(the_ode_solver, the_grid->num_active_cells, cur_time, ode_step, stimuli_configs);
 
-        update_monodomain(original_num_cells, the_grid->num_active_cells, the_grid->active_cells, beta, cm, dt_pde,
+
+        //update_monodomain(original_num_cells, the_grid->num_active_cells, the_grid->active_cells, beta, cm, dt_pde,
+        //                  the_ode_solver->sv, the_ode_solver->model_data.number_of_ode_equations, gpu);
+
+        if (!the_monodomain_solver->using_ddm)
+            update_monodomain(original_num_cells, the_grid->num_active_cells, the_grid->active_cells, beta, cm, dt_pde,
                           the_ode_solver->sv, the_ode_solver->model_data.number_of_ode_equations, gpu);
+        else
+            update_monodomain_ddm(original_num_cells, the_grid->num_active_cells, the_grid->active_cells, beta, cm,\
+                            the_monodomain_solver->kappa_x, the_monodomain_solver->kappa_y,the_monodomain_solver->kappa_z,\
+                            dt_pde, the_ode_solver->sv, the_ode_solver->model_data.number_of_ode_equations, gpu);
 
         ode_total_time += stop_stop_watch(&ode_time);
 
@@ -710,3 +727,163 @@ void configure_monodomain_solver_from_options(struct monodomain_solver *the_mono
     the_monodomain_solver->cm = options->cm;
     the_monodomain_solver->start_adapting_at = options->start_adapting_at;
 }
+
+// ***********************************************************************************************************
+// Berg and Pedro code ...
+void update_monodomain_ddm (uint32_t initial_number_of_cells, uint32_t num_active_cells, struct cell_node **active_cells, double beta, double cm,\
+                    const double kappa_x, const double kappa_y, const double kappa_z,\
+                    double dt_pde, real *sv, int n_equations_cell_model, bool use_gpu) 
+{
+
+    double alpha;
+
+#ifdef COMPILE_CUDA
+    real *vms = NULL;
+    size_t mem_size = initial_number_of_cells * sizeof(real);
+
+    if(use_gpu) 
+    {
+        vms = (real *)malloc(mem_size);
+        check_cuda_errors(cudaMemcpy(vms, sv, mem_size, cudaMemcpyDeviceToHost));
+    }
+#endif
+
+    int i;
+    
+    #pragma omp parallel for private(alpha)
+    for(i = 0; i < num_active_cells; i++) 
+    {
+        // 1) Calculate alpha for the diagonal element ...
+        alpha = ALPHA_CM(beta, cm, dt_pde, active_cells[i]->dx, active_cells[i]->dy, active_cells[i]->dz);
+        
+        if(use_gpu) 
+        {
+            #ifdef COMPILE_CUDA
+            active_cells[i]->b = vms[active_cells[i]->sv_position] * alpha;
+            #endif
+        } 
+        else 
+        {
+            active_cells[i]->b = sv[active_cells[i]->sv_position * n_equations_cell_model] * alpha;
+        }
+
+        // 2) Calculate kappas
+        // We need to capture the neighbours from the current volume 
+        struct element *cell_elements = active_cells[i]->elements;
+        uint32_t max_elements = sb_count(cell_elements);
+        
+        // Berg tip:
+        // TODO: The computation of the kappas will enter here ...
+        //       When we consider the anisotropic case
+
+        double dx = active_cells[i]->dx;
+        double dy = active_cells[i]->dy;
+        double dz = active_cells[i]->dz;
+
+        for (int j = 1; j < max_elements; j++)
+        {
+            int k = cell_elements[j].column;
+
+            if (cell_elements[j].direction == 'n') // North cell
+            {
+                double multiplier = (dx * dy) / dz;
+                if(use_gpu) 
+                {
+                    #ifdef COMPILE_CUDA
+                    active_cells[i]->b -= vms[active_cells[k]->sv_position] * multiplier * kappa_z / dt_pde;
+                    active_cells[i]->b += vms[active_cells[i]->sv_position] * multiplier * kappa_z / dt_pde;
+                    #endif
+                } 
+                else 
+                {
+                    active_cells[i]->b -= sv[active_cells[k]->sv_position * n_equations_cell_model] * multiplier * kappa_z / dt_pde;
+                    active_cells[i]->b += sv[active_cells[i]->sv_position * n_equations_cell_model] * multiplier * kappa_z / dt_pde;
+                }
+            }
+            else if (cell_elements[j].direction == 's') // South cell
+            {
+                double multiplier = (dx * dy) / dz;
+                if(use_gpu) 
+                {
+                    #ifdef COMPILE_CUDA
+                    active_cells[i]->b -= vms[active_cells[k]->sv_position] * multiplier * kappa_z / dt_pde;
+                    active_cells[i]->b += vms[active_cells[i]->sv_position] * multiplier * kappa_z / dt_pde;
+                    #endif
+                } 
+                else 
+                {
+                    active_cells[i]->b -= sv[active_cells[k]->sv_position * n_equations_cell_model] * multiplier * kappa_z / dt_pde;
+                    active_cells[i]->b += sv[active_cells[i]->sv_position * n_equations_cell_model] * multiplier * kappa_z / dt_pde;
+                }
+            }
+            else if (cell_elements[j].direction == 'e') // East cell
+            {
+                double multiplier = (dx * dz) / dy;
+                if(use_gpu) 
+                {
+                    #ifdef COMPILE_CUDA
+                    active_cells[i]->b -= vms[active_cells[k]->sv_position] * multiplier * kappa_y / dt_pde;
+                    active_cells[i]->b += vms[active_cells[i]->sv_position] * multiplier * kappa_y / dt_pde;
+                    #endif
+                } 
+                else 
+                {
+                    active_cells[i]->b -= sv[active_cells[k]->sv_position * n_equations_cell_model] * multiplier * kappa_y / dt_pde;
+                    active_cells[i]->b += sv[active_cells[i]->sv_position * n_equations_cell_model] * multiplier * kappa_y / dt_pde;
+                }
+            }
+            else if (cell_elements[j].direction == 'w') // West cell
+            {
+                double multiplier = (dx * dz) / dy;
+                if(use_gpu) 
+                {
+                    #ifdef COMPILE_CUDA
+                    active_cells[i]->b -= vms[active_cells[k]->sv_position] * multiplier * kappa_y / dt_pde;
+                    active_cells[i]->b += vms[active_cells[i]->sv_position] * multiplier * kappa_y / dt_pde;
+                    #endif
+                } 
+                else 
+                {
+                    active_cells[i]->b -= sv[active_cells[k]->sv_position * n_equations_cell_model] * multiplier * kappa_y / dt_pde;
+                    active_cells[i]->b += sv[active_cells[i]->sv_position * n_equations_cell_model] * multiplier * kappa_y / dt_pde;
+                }
+            }
+            else if (cell_elements[j].direction == 'f') // Forward cell
+            {
+                double multiplier = (dy * dz) / dx;
+                if(use_gpu) 
+                {
+                    #ifdef COMPILE_CUDA
+                    active_cells[i]->b -= vms[active_cells[k]->sv_position] * multiplier * kappa_x / dt_pde;
+                    active_cells[i]->b += vms[active_cells[i]->sv_position] * multiplier * kappa_x / dt_pde;
+                    #endif
+                } 
+                else 
+                {
+                    active_cells[i]->b -= sv[active_cells[k]->sv_position * n_equations_cell_model] * multiplier * kappa_x / dt_pde;
+                    active_cells[i]->b += sv[active_cells[i]->sv_position * n_equations_cell_model] * multiplier * kappa_x / dt_pde;
+                }
+            }
+            else if (cell_elements[j].direction == 'b') // Backward cell
+            {
+                double multiplier = (dy * dz) / dx;
+                if(use_gpu) 
+                {
+                    #ifdef COMPILE_CUDA
+                    active_cells[i]->b -= vms[active_cells[k]->sv_position] * multiplier * kappa_x / dt_pde;
+                    active_cells[i]->b += vms[active_cells[i]->sv_position] * multiplier * kappa_x / dt_pde;
+                    #endif
+                } 
+                else 
+                {
+                    active_cells[i]->b -= sv[active_cells[k]->sv_position * n_equations_cell_model] * multiplier * kappa_x / dt_pde;
+                    active_cells[i]->b += sv[active_cells[i]->sv_position * n_equations_cell_model] * multiplier * kappa_x / dt_pde;
+                }
+            }
+        }
+    }
+#ifdef COMPILE_CUDA
+    free(vms);
+#endif
+}
+// ***********************************************************************************************************
