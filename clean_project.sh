2c545011adf1bc910221c779a4bafc821616fbb1
Rafael Sachetto <rsachetto@gmail.com>
2019-03-13 11:48:03 -0300

    Build scripts update

--- clean_project.sh
+++ clean_project.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-cd build; rm -r *; cd ..
-cd bin; rm *
+
+BUILD_DIR="build"
+
+if [ "$#" -eq 1 ]; then
+    BUILD_DIR=$1
+fi
+
+if [ ! -d "${BUILD_DIR}" ]; then
+  echo "Directory ${BUILD_DIR} does not exist"
+  exit
+fi
+
+rm -fr ${BUILD_DIR}/*
+rm -fr bin/*
