cccca888ab4553c2065b5610f9365cf8dacbb6b1
bergolho <berg@ice.ufjf.br>
2019-04-03 18:55:23 -0300

    Add new Mitchell-Shaeffer celular model

---
+++ src/extra_data_library/extra_mixed_models.c
@@ -0,0 +1,13 @@
+//
+// Created by berg on 03/04/19.
+//
+
+#include "../config/extra_data_config.h"
+#include "../libraries_common/config_helpers.h"
+#include "../libraries_common/common_data_structures.h"
+
+SET_EXTRA_DATA (set_extra_data_for_half_domain_plain)
+{
+    printf("[extra_data] Inside 'set_extra_data_for_half_domain_plain'\n");
+    exit(EXIT_SUCCESS);
+}
