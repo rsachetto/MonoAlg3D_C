fff8e132a8f87338faed7486db717fe9d494101f
Rafael Sachetto <rsachetto@gmail.com>
2019-04-17 16:23:24 -0300

    Changes on the visualization code.

--- src/raylib/src/raylib.h
+++ src/raylib/src/raylib.h
@@ -1206,10 +1206,8 @@ RLAPI int TextToInteger(const char *text);                            // Get int
 // Basic geometric 3D shapes drawing functions
 RLAPI void DrawLine3D(Vector3 startPos, Vector3 endPos, Color color);                                    // Draw a line in 3D world space
 RLAPI void DrawCircle3D(Vector3 center, float radius, Vector3 rotationAxis, float rotationAngle, Color color); // Draw a circle in 3D world space
-RLAPI void DrawCubes(Cube *cubes, int numCubes);
 RLAPI void DrawCube(Vector3 position, float width, float height, float length, Color color);             // Draw cube
 RLAPI void DrawCubeV(Vector3 position, Vector3 size, Color color);                                       // Draw cube (Vector version)
-void DrawCubesWires(Cube *cubes, int numCubes, bool useColor);
 RLAPI void DrawCubeWires(Vector3 position, float width, float height, float length, Color color);        // Draw cube wires
 RLAPI void DrawCubeWiresV(Vector3 position, Vector3 size, Color color);                                  // Draw cube wires (Vector version)
 RLAPI void DrawCubeTexture(Texture2D texture, Vector3 position, float width, float height, float length, Color color); // Draw cube textured
