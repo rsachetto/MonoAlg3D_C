885fbc6374928656e7a8b3ee12db9f09c7b3c844
bergolho <berg@ice.ufjf.br>
2019-07-23 13:58:46 -0300

    Elnaz TT-2004 sustained spiral wave is finally working

--- scripts/animations/convert_png_to_mp4.sh
+++ scripts/animations/convert_png_to_mp4.sh
@@ -6,10 +6,10 @@
 
 # Variables
 FILENAME="frames/frame"
-FRAME_RATE="100"
-END_FRAME="2000"
-OUTPUT_VIDEO_FILENAME="video/plain_mesh_tentusscher_2004_spiral_wave"
-RESOLUTION="1030x430"
+FRAME_RATE="50"
+END_FRAME="500"
+OUTPUT_VIDEO_FILENAME="video/elnaz_plain_spiral_wave_comparison"
+RESOLUTION="1030x680"
 
 # Execute the converting command using FFMPEG
 ffmpeg -r ${FRAME_RATE} -f image2 -s ${RESOLUTION} -start_number 1 -i ${FILENAME}.%04d.png -vframes ${END_FRAME} -vcodec libx264 -crf 25  -pix_fmt yuv420p ${OUTPUT_VIDEO_FILENAME}.mp4
