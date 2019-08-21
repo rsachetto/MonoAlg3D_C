dbbe6632121f3262bb7d7a9bb0a9872bb7e383ad
Rafael Sachetto <rsachetto@gmail.com>
2019-08-21 10:52:24 -0300

    Correcting bugs on the save activation times function

--- src/config/save_mesh_config.h
+++ src/config/save_mesh_config.h
@@ -12,6 +12,26 @@
 #define SAVE_MESH(name) EXPORT_FN void name(struct config *config, struct grid *the_grid, int iteration_count, real_cpu current_t, real_cpu last_t, real dt)
 typedef SAVE_MESH(save_mesh_fn);
 
+#define INIT_SAVE_MESH(name) EXPORT_FN void name(struct config *config)
+typedef INIT_SAVE_MESH(init_save_mesh_fn);
+
+#define END_SAVE_MESH(name) EXPORT_FN void name(struct config *config)
+typedef END_SAVE_MESH(end_save_mesh_fn);
+
+#define CALL_INIT_SAVE_MESH(config)                                                                          \
+    do {                                                                                                               \
+        if(config->init_function) {                                                                                    \
+            ((init_save_mesh_fn*) config->init_function)(config);                                     \
+        }                                                                                                              \
+    } while(0)
+
+#define CALL_END_SAVE_MESH(config)                                                                                 \
+    do {                                                                                                               \
+        if(config->end_function) {                                                                                     \
+            ((end_save_mesh_fn*)config->end_function)(config);                                              \
+        }                                                                                                              \
+    } while(0)
+
 void print_save_mesh_config_values(struct config* s);
 
 #endif //MONOALG3D_SAVE_MESH_CONFIG_H
