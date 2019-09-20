c7c555ff7efe127290cea5983d6764cbbc9fc84d
Rafael Sachetto <rsachetto@gmail.com>
2019-09-20 14:42:53 -0300

    Use with caution

--- src/restore_state_library/build.sh
+++ src/restore_state_library/build.sh
@@ -2,4 +2,4 @@ if [ -n "$CUDA_FOUND" ]; then
     EXTRA_CUDA_LIBS="cudart"
 fi
 
-COMPILE_SHARED_LIB "default_restore_state" "restore_state.c" "" "alg config_helpers utils" "$EXTRA_CUDA_LIBS" "$CUDA_LIBRARY_PATH"
+COMPILE_SHARED_LIB "default_restore_state" "restore_state.c" "" "alg config_helpers utils string" "$EXTRA_CUDA_LIBS" "$CUDA_LIBRARY_PATH"
