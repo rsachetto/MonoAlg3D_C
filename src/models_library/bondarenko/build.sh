94d5893d3d6b20b5d75e426c3a85368b886a1d1e
Rafael Sachetto <rsachetto@gmail.com>
2021-03-25 10:08:37 -0300

    Correcting a small error in the bondarenko model build file

--- src/models_library/bondarenko/build.sh
+++ src/models_library/bondarenko/build.sh
@@ -3,5 +3,5 @@ MODEL_FILE_CPU="bondarenko_2004.c"
 MODEL_FILE_GPU="bondarenko_2004_GPU.cu"
 COMMON_HEADERS="bondarenko_2004.h"
 
-COMPILE_MODEL_LIB "bondarenko_2004" "$MODEL_FILE_CPU" "$MODEL_FILE_GPU" "$COMMON_HEADERS" "-I$CUDA)"
+COMPILE_MODEL_LIB "bondarenko_2004" "$MODEL_FILE_CPU" "$MODEL_FILE_GPU" "$COMMON_HEADERS"
 ###########################################################
