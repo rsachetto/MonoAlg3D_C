3f11c710775ef5d4b62f9716adacc06a523d5a06
bergolho <berg@ice.ufjf.br>
2021-04-05 11:04:43 -0300

    Add more examples using the ToRORd model

--- src/extra_data_library/extra_data.c
+++ src/extra_data_library/extra_data.c
@@ -181,3 +181,184 @@ SET_EXTRA_DATA (set_mixed_model_purkinje_and_tissue)
 
     return (void*)mapping;
 }
+
+SET_EXTRA_DATA(set_extra_data_mixed_model_epi_mid_endo) {
+
+    uint32_t num_eq = 43;   // ToRORd number of equations
+	uint32_t num_active_cells = the_grid->num_active_cells;
+	real side_length = the_grid->mesh_side_length.x;
+
+	// The percentages were taken from the ToRORd paper (Transmural experiment)
+	real side_length_endo = side_length*0.45;
+	real side_length_mid = side_length_endo + side_length*0.25;
+	real side_length_epi = side_length_mid + side_length*0.3;
+
+    // The extra data size is the initial state vector for each celltype plus the mapping of each cell
+	*extra_data_size = sizeof(real)*(num_active_cells) + sizeof(real)*num_eq*3;
+
+    real *extra_data = (real*)malloc(*extra_data_size);
+
+    // Set the initial conditions (celltype=ENDO)
+    int offset = 0;
+    extra_data[0] = -88.7638;
+    extra_data[1] = 0.0111;
+    extra_data[2] = 7.0305e-5;
+    extra_data[3] = 12.1025;
+    extra_data[4] = 12.1029;
+    extra_data[5] = 142.3002;
+    extra_data[6] = 142.3002;
+    extra_data[7] = 1.5211;
+    extra_data[8] = 1.5214;
+    extra_data[9] = 8.1583e-05;
+    extra_data[10] = 8.0572e-4;
+    extra_data[11] = 0.8286;
+    extra_data[12] = 0.8284;
+    extra_data[13] = 0.6707;
+    extra_data[14] = 0.8281;
+    extra_data[15] = 1.629e-4;
+    extra_data[16] = 0.5255;
+    extra_data[17] = 0.2872;
+    extra_data[18] = 9.5098e-4;
+    extra_data[19] = 0.9996;
+    extra_data[20] = 0.5936;
+    extra_data[21] = 4.8454e-4;
+    extra_data[22] = 0.9996;
+    extra_data[23] = 0.6538;
+    extra_data[24] = 8.1084e-9;
+    extra_data[25] = 1.0;
+    extra_data[26] = 0.939;
+    extra_data[27] = 1.0;
+    extra_data[28] = 0.9999;
+    extra_data[29] = 1.0;
+    extra_data[30] = 1.0;
+    extra_data[31] = 1.0;
+    extra_data[32] = 6.6462e-4;
+    extra_data[33] = 0.0012;
+    extra_data[34] = 7.0344e-4;
+    extra_data[35] = 8.5109e-4;
+    extra_data[36] = 0.9981;
+    extra_data[37] = 1.3289e-5;
+    extra_data[38] = 3.7585e-4;
+    extra_data[39] = 0.248;
+    extra_data[40] = 1.7707e-4;
+    extra_data[41] = 1.6129e-22;
+    extra_data[42] = 1.2475e-20;
+
+    // Set the initial conditions (celltype=EPI)
+    offset = num_eq;
+    extra_data[0+offset] = -89.1400;
+    extra_data[1+offset] = 0.0129;
+    extra_data[2+offset] = 5.7672e-05;
+    extra_data[3+offset] = 12.8363;
+    extra_data[4+offset] = 12.8366;
+    extra_data[5+offset] = 142.6951;
+    extra_data[6+offset] = 142.6951;
+    extra_data[7+offset] = 1.8119;
+    extra_data[8+offset] = 1.8102;
+    extra_data[9+offset] = 6.6309e-05;
+    extra_data[10+offset] = 0.00074303;
+    extra_data[11+offset] = 0.8360;
+    extra_data[12+offset] = 0.8359;
+    extra_data[13+offset] = 0.6828;
+    extra_data[14+offset] = 0.8357;
+    extra_data[15+offset] = 0.00015166;
+    extra_data[16+offset] = 0.5401;
+    extra_data[17+offset] = 0.3034;
+    extra_data[18+offset] = 0.00092716;
+    extra_data[19+offset] = 0.9996;
+    extra_data[20+offset] = 0.9996;
+    extra_data[21+offset] = 0.0004724;
+    extra_data[22+offset] = 0.9996;
+    extra_data[23+offset] = 0.9996;
+    extra_data[24+offset] = 0;
+    extra_data[25+offset] = 1.0;
+    extra_data[26+offset] = 0.9485;
+    extra_data[27+offset] = 1.0;
+    extra_data[28+offset] = 0.9999;
+    extra_data[29+offset] = 1.0;
+    extra_data[30+offset] = 1.0;
+    extra_data[31+offset] = 1.0;
+    extra_data[32+offset] = 0.00030853;
+    extra_data[33+offset] = 0.00053006;
+    extra_data[34+offset] = 0.00067941;
+    extra_data[35+offset] = 0.00082869;
+    extra_data[36+offset] = 0.9982;
+    extra_data[37+offset] = 9.5416e-06;
+    extra_data[38+offset] = 0.00027561;
+    extra_data[39+offset] = 0.2309;
+    extra_data[40+offset] = 0.00016975;
+    extra_data[41+offset] = 2.8189e-24;
+    extra_data[42+offset] = 0;
+
+    // Set the initial conditions (celltype=MID)
+    offset = num_eq*2;
+    extra_data[0+offset] = -89.1704;
+    extra_data[1+offset] = 0.0192;
+    extra_data[2+offset] = 6.5781e-05;
+    extra_data[3+offset] = 15.0038;
+    extra_data[4+offset] = 15.0043;
+    extra_data[5+offset] = 143.0403;
+    extra_data[6+offset] = 143.0402;
+    extra_data[7+offset] = 1.9557;
+    extra_data[8+offset] = 1.9593;
+    extra_data[9+offset] = 8.166e-05;
+    extra_data[10+offset] = 0.00073818;
+    extra_data[11+offset] = 0.8365;
+    extra_data[12+offset] = 0.8363;
+    extra_data[13+offset] = 0.6838;
+    extra_data[14+offset] = 0.8358;
+    extra_data[15+offset] = 0.00015079;
+    extra_data[16+offset] = 0.5327;
+    extra_data[17+offset] = 0.2834;
+    extra_data[18+offset] = 0.00092527;
+    extra_data[19+offset] = 0.9996;
+    extra_data[20+offset] = 0.5671;
+    extra_data[21+offset] = 0.00047143;
+    extra_data[22+offset] = 0.9996;
+    extra_data[23+offset] = 0.6261;
+    extra_data[24+offset] = 0;
+    extra_data[25+offset] = 1.0;
+    extra_data[26+offset] = 0.92;
+    extra_data[27+offset] = 1.0;
+    extra_data[28+offset] = 0.9998;
+    extra_data[29+offset] = 1.0;
+    extra_data[30+offset] = 1.0;
+    extra_data[31+offset] = 1.0;
+    extra_data[32+offset] = 0.00051399;
+    extra_data[33+offset] = 0.0012;
+    extra_data[34+offset] = 0.00069560;
+    extra_data[35+offset] = 0.00082672;
+    extra_data[36+offset] = 0.9979;
+    extra_data[37+offset] = 1.8784e-05;
+    extra_data[38+offset] = 0.00054206;
+    extra_data[39+offset] = 0.2653;
+    extra_data[40+offset] = 0.00016921;
+    extra_data[41+offset] = 0;
+    extra_data[42+offset] = 0;
+
+    offset = num_eq*3;
+
+	struct cell_node ** ac = the_grid->active_cells;
+
+	int i;
+
+	OMP(parallel for)
+	for (i = 0; i < num_active_cells; i++) {
+
+		real center_x = ac[i]->center.x;
+
+		// ENDO
+		if (center_x < side_length_endo) 
+			extra_data[i+offset] = 0.0;
+		// MID
+		else if (center_x >= side_length_endo && center_x < side_length_mid)
+			extra_data[i+offset] = 2.0;
+		// EPI
+		else
+			extra_data[i+offset] = 1.0;
+
+	}
+
+	return (void*)extra_data;
+
+}
