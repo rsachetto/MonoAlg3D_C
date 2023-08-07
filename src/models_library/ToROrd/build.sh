14ce933978855f9b09730b69e27c9d71ad094975
bergolho <berg@ice.ufjf.br>
2023-08-07 15:01:41 +0100

    Update benchmark Purkinje coupling simulation

--- src/models_library/ToROrd/build.sh
+++ src/models_library/ToROrd/build.sh
@@ -20,8 +20,8 @@ COMMON_HEADERS="ToRORd_dynCl_mixed_endo_mid_epi.h"
 COMPILE_MODEL_LIB "ToRORd_dynCl_mixed_endo_mid_epi" "$MODEL_FILE_CPU" "$MODEL_FILE_GPU" "$COMMON_HEADERS"
 
 ############## Paci_ToRORd dynCl PhiCaL IKCa Mixed Apicobasal InfarctRemod ##############################
-MODEL_FILE_CPU="Paci_ToRORd_dynCl_PhiCaL_IKCa_mixed_apicobasal_infarctionRemod_RL.c"
-MODEL_FILE_GPU="Paci_ToRORd_dynCl_PhiCaL_IKCa_mixed_apicobasal_infarctionRemod_RL.cu"
-COMMON_HEADERS="Paci_ToRORd_dynCl_PhiCaL_IKCa_mixed_apicobasal_infarctionRemod_RL.h"
+#MODEL_FILE_CPU="Paci_ToRORd_dynCl_PhiCaL_IKCa_mixed_apicobasal_infarctionRemod_RL.c"
+#MODEL_FILE_GPU="Paci_ToRORd_dynCl_PhiCaL_IKCa_mixed_apicobasal_infarctionRemod_RL.cu"
+#COMMON_HEADERS="Paci_ToRORd_dynCl_PhiCaL_IKCa_mixed_apicobasal_infarctionRemod_RL.h"
 
-COMPILE_MODEL_LIB "Paci_ToRORd_dynCl_PhiCaL_IKCa_mixed_apicobasal_infarctionRemod_RL" "$MODEL_FILE_CPU" "$MODEL_FILE_GPU" "$COMMON_HEADERS"
+#COMPILE_MODEL_LIB "Paci_ToRORd_dynCl_PhiCaL_IKCa_mixed_apicobasal_infarctionRemod_RL" "$MODEL_FILE_CPU" "$MODEL_FILE_GPU" "$COMMON_HEADERS"
