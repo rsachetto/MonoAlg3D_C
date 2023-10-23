037a42b52e534644c65d74bc8353ea75333b0893
Rafael Sachetto <rsachetto@gmail.com>
2023-10-23 11:30:44 -0300

    Small code change

--- src/main_simulator.c
+++ src/main_simulator.c
@@ -115,7 +115,7 @@ void free_current_simulation_resources(struct user_options *options, struct mono
 }
 
 #ifdef COMPILE_GUI
-static void init_gui_config_for_simulation(struct user_options *options, struct gui_shared_info *gui_config, bool only_restart) {
+static void init_gui_config_for_simulation(const struct user_options *options, struct gui_shared_info *gui_config, bool only_restart) {
 
     if(!only_restart) {
         omp_init_lock(&gui_config->draw_lock);
@@ -176,12 +176,12 @@ int main(int argc, char **argv) {
     log_warn("Running the debug version. If you do not need debug information, build with './build -r' or 'make release' to improve the performace!\n");
 #endif
 
+#if defined(_OPENMP)
     int np = monodomain_solver->num_threads;
 
     if(np == 0)
         np = 1;
 
-#if defined(_OPENMP)
     omp_set_num_threads(np);
 #endif
 
