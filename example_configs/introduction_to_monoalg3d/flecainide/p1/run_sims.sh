97e3677fd5d430804a28124cd91060ec8dc6d799
carl <63507872+crlgb@users.noreply.github.com>
2024-03-13 15:35:12 +0000

    fix sh script

--- example_configs/introduction_to_monoalg3d/flecainide/p1/run_sims.sh
+++ example_configs/introduction_to_monoalg3d/flecainide/p1/run_sims.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-files = (
+files=(
     example_configs/introduction_to_monoalg3d/flecainide/p1/EX00_locally_applied_flecainide_0_1_p1.ini
     example_configs/introduction_to_monoalg3d/flecainide/p1/EX00_locally_applied_flecainide_1_p1.ini
     example_configs/introduction_to_monoalg3d/flecainide/p1/EX00_locally_applied_flecainide_75_p1.ini
     example_configs/introduction_to_monoalg3d/flecainide/p1/EX00_locally_applied_flecainide_reference.ini
 )
 
-for file in *; do
+for file in "${files[@]}"; do
     if [[ -f $file ]]; then
         ./bin/MonoAlg3D -c "$file"
     fi
