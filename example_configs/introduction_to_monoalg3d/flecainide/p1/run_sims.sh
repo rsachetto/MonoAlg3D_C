4197d59349e4e4f0213599dbe30f57426139b0a7
carl <63507872+crlgb@users.noreply.github.com>
2024-03-13 15:32:00 +0000

    add 3 dosages and reference

---
+++ example_configs/introduction_to_monoalg3d/flecainide/p1/run_sims.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+files = (
+    example_configs/introduction_to_monoalg3d/flecainide/p1/EX00_locally_applied_flecainide_0_1_p1.ini
+    example_configs/introduction_to_monoalg3d/flecainide/p1/EX00_locally_applied_flecainide_1_p1.ini
+    example_configs/introduction_to_monoalg3d/flecainide/p1/EX00_locally_applied_flecainide_75_p1.ini
+    example_configs/introduction_to_monoalg3d/flecainide/p1/EX00_locally_applied_flecainide_reference.ini
+)
+
+for file in *; do
+    if [[ -f $file ]]; then
+        ./bin/MonoAlg3D -c "$file"
+    fi
+done
