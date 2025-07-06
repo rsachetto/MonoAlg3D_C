ad8df23e333876b40941309db75288c61c24c962
Lucas <berg@ice.ufjf.br>
2025-07-06 09:44:42 -0300

    Update on 'save_one_cell_state_variables' function to save steady-state and specified state-vector after a certain time

--- scripts/plot_potential.py
+++ scripts/plot_potential.py
@@ -2,48 +2,33 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def read_transmembrane_potential(input_file, dt, print_rate):
-	data = np.genfromtxt(input_file)
-	n = len(data)
-
-	timesteps = np.arange(0,n)*dt*print_rate
-	vms = data
-	
-	return timesteps, vms
-
-
 def plot_transmembrane_potential(t, v):
-	plt.grid()
-	plt.plot(t, v, label="Vm", c="black", linewidth=3.0)
+	plt.plot(t, v, label="Vm", c="black", linewidth=1.0)
 	plt.xlabel("t (ms)",fontsize=15)
 	plt.ylabel("V (mV)",fontsize=15)
-	plt.title("Action potential - Purkinje cell",fontsize=14)
+	plt.title("Action potential",fontsize=14)
 	plt.legend(loc=0,fontsize=14)
 	plt.show()
 	#plt.savefig("ap.pdf")
 
-
 def main():
 	
-	if len(sys.argv) != 4:
+	if len(sys.argv) != 2:
 		print("-------------------------------------------------------------------------")
-		print("Usage:> python %s <input_file> <dt> <print_rate>" % sys.argv[0])
+		print("Usage:> python %s <input_file>" % sys.argv[0])
 		print("-------------------------------------------------------------------------")
-		print("<input_file> = Input file with the AP's from each timestep")
-		print("<dt> = Timestep value used for the simulation")
-		print("<print_rate> = Print rate used for the simulation")
+		print("<input_file> = Input file with the state vector from each timestep")
 		print("-------------------------------------------------------------------------")
 		return 1
 
 	input_file = sys.argv[1]
-	dt = float(sys.argv[2])
-	print_rate = int(sys.argv[3])
 
-	t, vm = read_transmembrane_potential(input_file,dt,print_rate)
+	sv = np.genfromtxt(input_file)
+
+	t = sv[:,0]
+	vm = sv[:,1]
 
 	plot_transmembrane_potential(t,vm)
-
 
 if __name__ == "__main__":
 	main()
